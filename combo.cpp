36f233660c034994584b0cf9dfbeb42cbcf4f1f7
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-02-20 13:14:08 -0500

    Update combo.cpp

--- combo.cpp
+++ combo.cpp
@@ -10,21 +10,32 @@ LANG: C++
 int main() {
 	int dialNumbers;
 	int farmer[3]; int master[3];
-	int x; int y; int z;
+	int x=0; int y=0; int z=0;
+	int firstDifference, int secondDifference; int thirdDifference;
 	int solutions=250;
 	std::ifstream comboIn("combo.in");
 	comboIn >> dialNumbers;
 	comboIn >> farmer[0] >> farmer[1] >> farmer[2];
 	comboIn >> master[0] >> master[1] >> master[2];
 	comboIn.close();
+	
+	firstDifference = std::abs(farmer[0] - master[0]);
+	secondDifference = std::abs(farmer[1] - master[1]);
+	thirdDifference = std::abs(farmer[2] - master[2]);
 
-	if (std::abs(farmer[0] - master[0]) < 4) {
-	
+	if (firstDifference <= 4) {
+		x = 5 - firstDifference;
 	}
-	if (std::abs(farmer[1] - master[1]) < 4) {
-	
+	if (secondDifference <= 4) {
+		y = 5 - secondDifference;
 	}
-	if (std::abs(farmer[2] - master[2]) < 4) {
+	if (std::abs(farmer[2] - master[2]) <= 4) {
+		z = 5 - thirdDifference;
+	}
+	
+	solutions -= x*y*z;
+	
+	if (dialNumbers < 5) {
 	
 	}
 
