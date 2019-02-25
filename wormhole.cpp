c19c3101ad819664a1bb8057c1700b549648fc1f
angela-zhang1 <44886506+angela-zhang1@users.noreply.github.com>
2019-02-25 11:14:53 -0500

    Update wormhole.cpp

--- wormhole.cpp
+++ wormhole.cpp
@@ -5,16 +5,23 @@ LANG: C++
 */
 
 #include <fstream>
-#include <vector>
 #include <array>
 
 int main() {
 	int wormholeCount;
 	int pairings = 0;
+	int x; int y; 
 	std::ifstream wormholeIn("wormhole.in");
+	std::array<std::array<int, 2>, 12> wormholes;
 	wormholeIn >> wormholeCount;
-	for (int i = 0; i < wormholeCount; i++) {}
+	for (int i = 0; i < wormholeCount; i++) {
+		wormholeIn >> x >> y;
+		wormholes[i][0] = x;
+		wormholes[i][1] = y;
+	}
 	wormholeIn.close();
+
+	for (int i = 0; i < wormholeCount; i++) {}
 
 
 	std::ofstream wormholeOut("wormhole.out");
